e02c755d5d2f60ed79766a3d180a9883225fd632
daianan <1224325563@qq.com>
2017-11-29 10:59:24 +0800

    pages tiaozhuan

---
+++ zuiyouApp/js/search.js
@@ -0,0 +1,10 @@
+$(function(){
+	var searchNavs=$("#searchNav a");
+	var smallWidth=$("#small").innerWidth()/2;
+	var smallLeft=searchNavs.eq(0).innerWidth()/2-smallWidth;
+	$("#small").css("left",smallLeft);
+	searchNavs.on("tap",function(){
+		var small_left=$(this).innerWidth()*($(this).index()+1)-$(this).innerWidth()/2-smallWidth;
+		$("#small").animate({"left":small_left},200);
+	});
+});
