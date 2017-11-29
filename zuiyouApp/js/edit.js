0a6e73c1c42a450eac06c585730796335bed37b8
daianan <1224325563@qq.com>
2017-11-29 15:40:40 +0800

    r32

---
+++ zuiyouApp/js/edit.js
@@ -0,0 +1,34 @@
+$(function() {
+	$.fn.autoTextarea = function(options) {
+		var defaults = {
+			maxHeight: null,
+			minHeight: $(this).height()
+		};
+		var opts = $.extend({}, defaults, options);
+		return $(this).each(function() {
+			var _this = $(this);
+			_this.bind('paste cur keydown keyup focus blur', function() {
+				var height, style = this.style;
+				style.height = opts.minHeight + 'px';
+				if(this.scrollHeight > opts.minHeight) {
+					if(opts.maxHeight && this.scrollHeight > opts.maxHeight) {
+						height = opts.maxHeight;
+						style.overflowY = 'scroll';
+					} else {
+						height = this.scrollHeight;
+						style.overflowY = 'hidden';
+					}
+					style.height = height + 'px';
+				}
+			});
+		});
+	}
+	$(function() {
+		$('#editTextarea').autoTextarea({
+			maxHeight: 300
+		});
+	});
+	$("#editClose").on("tap", function() {
+		history.back();
+	});
+});
