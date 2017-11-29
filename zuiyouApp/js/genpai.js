bcc4c18be49d0e22690c389324c73f322ef7ad7f
daianan <1224325563@qq.com>
2017-11-29 11:16:02 +0800

    dqwrfq

--- zuiyouApp/js/genpai.js
+++ zuiyouApp/js/genpai.js
@@ -1,4 +1,7 @@
-$(function(){
-	var footerNavs=$("#footerNav li a");
+$(function() {
+	var footerNavs = $("#footerNav li a");
 	footerNavs.eq(1).addClass('footerNavSel').siblings('li').removeClass('footerNavSel');
+	$("#search").on('tap', function() {
+		window.location.href = "http://daianan.github.io/phonenew/zuiyouApp/search.html";
+	});
 });
--- zuiyouApp/js/search.js
+++ zuiyouApp/js/search.js
@@ -1,10 +1,16 @@
-$(function(){
-	var searchNavs=$("#searchNav a");
-	var smallWidth=$("#small").innerWidth()/2;
-	var smallLeft=searchNavs.eq(0).innerWidth()/2-smallWidth;
-	$("#small").css("left",smallLeft);
-	searchNavs.on("tap",function(){
-		var small_left=$(this).innerWidth()*($(this).index()+1)-$(this).innerWidth()/2-smallWidth;
-		$("#small").animate({"left":small_left},200);
+$(function() {
+	var searchNavs = $("#searchNav a");
+	var smallWidth = $("#small").innerWidth() / 2;
+	var smallLeft = searchNavs.eq(0).innerWidth() / 2 - smallWidth;
+	$("#small").css("left", smallLeft);
+	$("#searchTxt").trigger("focus");
+	searchNavs.on("tap", function() {
+		var small_left = $(this).innerWidth() * ($(this).index() + 1) - $(this).innerWidth() / 2 - smallWidth;
+		$("#small").animate({
+			"left": small_left
+		}, 200);
+	});
+	$("#searchClose").on("tap", function() {
+		history.back();
 	});
 });
