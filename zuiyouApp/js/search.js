916cd6f033f6f74323b9a8888d528fd62df77768
daianan <1224325563@qq.com>
2017-11-29 11:23:52 +0800

    rwerw

--- zuiyouApp/js/search.js
+++ zuiyouApp/js/search.js
@@ -3,12 +3,14 @@ $(function() {
 	var smallWidth = $("#small").innerWidth() / 2;
 	var smallLeft = searchNavs.eq(0).innerWidth() / 2 - smallWidth;
 	$("#small").css("left", smallLeft);
+	searchNavs.eq(0).addClass("col");
 	$("#searchTxt").trigger("focus");
 	searchNavs.on("tap", function() {
 		var small_left = $(this).innerWidth() * ($(this).index() + 1) - $(this).innerWidth() / 2 - smallWidth;
 		$("#small").animate({
 			"left": small_left
 		}, 200);
+		$(this).addClass("col").siblings('a').removeClass("col");
 	});
 	$("#searchClose").on("tap", function() {
 		history.back();
