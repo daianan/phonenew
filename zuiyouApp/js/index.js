f4c206bb813f93e98cec5616a27aab2ab5932581
daianan <1224325563@qq.com>
2017-11-29 15:38:02 +0800

    erwgege

--- zuiyouApp/js/index.js
+++ zuiyouApp/js/index.js
@@ -4,11 +4,15 @@ $(function() {
 	$("#search").on('tap', function() {
 		window.location.href = "http://daianan.github.io/phonenew/zuiyouApp/search.html";
 	});
-	
+	$("#edit").on('tap', function() {
+		window.location.href = "http://daianan.github.io/phonenew/zuiyouApp/edit.html";
+	});
 	var indexCloses=$("#tieList1 b[class=close]");
 	var indexLists1=$("#tieList1 > li");
 	indexCloses.on("tap",function(){
 		var bIndex=$(this).parent().parent().index();
-		$(".tieList1").children().eq(bIndex).remove();
+		indexLists1.eq(bIndex).slideToggle(500);
+//		indexLists1.eq(bIndex).remove();
+		
 	});
 });
