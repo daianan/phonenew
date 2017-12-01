2082a6867d46ff733c501dd49b622344b1b12e0e
daianan <1224325563@qq.com>
2017-12-01 15:47:09 +0800

    rewrt

--- zuiyouApp/js/genpai.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	var footerNavs = $("#footerNav li a");
-	footerNavs.eq(1).addClass('footerNavSel').siblings('li').removeClass('footerNavSel');
-	$("#search").on('tap', function() {
-		window.location.href = "http://daianan.github.io/phonenew/zuiyouApp/search.html";
-	});
-});
