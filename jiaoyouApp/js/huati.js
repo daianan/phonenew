76ece4be1946240eedc3d035a9592174bdde1a6a
daianan <1224325563@qq.com>
2017-12-05 09:47:37 +0800

    rwetwet

--- jiaoyouApp/js/huati.js
+++ jiaoyouApp/js/huati.js
@@ -7,21 +7,40 @@ $(function() {
 		window.location.href = "http://daianan.github.io/phonenew/jiaoyouApp/search.html";
 	});
 	
+	//导航点击
 	var huatiNavs=$("#huatiNav a");
 	var huatiSels=$("#huatiSel ul");
 	huatiSels.fadeOut().eq(0).fadeIn();
-	console.log(huatiNavs.eq(0).width()/2-$("#slider").width()/2)
 	$("#slider").css("left",huatiNavs.eq(0).width()/2-$("#slider").width()/2);
 	huatiNavs.eq(0).addClass("sel").siblings('a').removeClass('sel');
 	huatiNavs.on("tap",function(){
-		
+		$(this).addClass("sel").siblings('a').removeClass('sel');
+		$("#slider").animate({"left":$(this).width()*$(this).index()+$(this).width()/2-$("#slider").width()/2},100);
 	});
 	
-	
+	//左滑、右滑
 	var huatiNav = document.getElementById("huatiNav");
 	huatiNav.addEventListener("touchstart", function(e) {
 		e.preventDefault();
 	}); //阻止默认行为
-	
+	var n=0;
+	$(document).on("swipeLeft",function(){
+		n++;
+		$("#huatiSel ul").eq(n).fadeIn().siblings("ul").fadeOut();
+		if (n>=3) {
+			n=3;
+		}
+		huatiNavs.eq(n).addClass("sel").siblings('a').removeClass('sel');
+		$("#slider").animate({"left":huatiNavs.eq(n).width()*n+huatiNavs.eq(n).width()/2-$("#slider").width()/2},100);
+	});
+	$(document).on("swipeRight",function(){
+		n--;
+		$("#huatiSel ul").eq(n).fadeIn().siblings("ul").fadeOut();
+		if (n<=0) {
+			n=0;
+		}
+		huatiNavs.eq(n).addClass("sel").siblings('a').removeClass('sel');
+		$("#slider").animate({"left":huatiNavs.eq(n).width()*n+huatiNavs.eq(n).width()/2-$("#slider").width()/2},100);
+	});
 	
 });
